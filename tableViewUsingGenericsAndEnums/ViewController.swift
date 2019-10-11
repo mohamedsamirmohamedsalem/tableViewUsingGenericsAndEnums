bd8f9234be1063eb5c8ceac19f20218df63e19de
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-10-11 18:04:05 +0200

    Initial Commit

---
+++ tableViewUsingGenericsAndEnums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewUsingGenericsAndEnums
+//
+//  Created by Mohamed Samir on 10/11/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
